c1bcb37667d6026ab0bf0ceccf191ebe1110d5f3
venky1603 <51037901+venky1603@users.noreply.github.com>
2019-08-03 23:03:03 -0400

    Ex12.01 Networks and Sockets

---
+++ py4e/ex_12_01.py
@@ -0,0 +1,16 @@
+#Ex12.01 Networks and Sockets
+import socket
+print('Ex 12.01')
+
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect(('data.pr4e.org', 80))
+cmd = 'GET http://data.pr4e.org/intro-short.txt HTTP/1.0\r\n\r\n'.encode()
+mysock.send(cmd)
+
+while True:
+    data = mysock.recv(512)
+    if len(data) < 1:
+        break
+    print(data.decode(),end='')
+
+mysock.close()
